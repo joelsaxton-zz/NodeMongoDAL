0f3b0932ffc0fdd02be8ef453e94da8c4a88adb4
Frokenstein <frokenstein@gmail.com>
2014-11-07 22:03:53 -0800

    master added default _id routes

--- routes/rest.js
+++ routes/rest.js
@@ -1,6 +1,134 @@
-module.exports.loadRestRoutes = function(app, configCollection, db) {
+module.exports.loadRestRoutes = function(app, configCollection, db, configtable, idroutes, requireauth, objectid) {
 
-// Load REST API routes from config table and create Express routes
+    // Load basic (non-custom) _id based routes if idroute params is true
+    loadBasicIdRoutes(app, db, configtable, idroutes, requireauth, objectid);
+
+    // Load administrator defined dynamic routes
+    loadAdminDefinedRoutes(app, configCollection, db, requireauth);
+
+}
+
+
+// Create basic _id GET, POST, PUT routes @todo - DELETE routes
+function loadBasicIdRoutes(app, db, configtable, idroutes, requireauth, objectid){
+    // If option enabled create _id routes
+    if (idroutes){
+        console.log('Configuration value "idroutes" set to true, default routes will be loaded');
+        // Get list of collections for _id GET/PUT routes
+        db.collectionNames(function(err, names){
+            var collections = [];
+            for (var name in names){
+                var coll = names[name].name.split('.')[1];
+                if (coll != 'system' && coll != configtable){
+                    collections.push(coll);
+                }
+            }
+            for (var route in collections){
+                // Set vars
+                var collection = db.collection(collections[route]);
+
+                // Create Express GET route
+                app.get('/' + collections[route] + '/:id', function (req, res) {
+                    // Return 400 Bad Request error if no params provided
+                    if (typeof req.params.id == 'undefined') {
+                        return res.status(400).send({success: false, code: 400, error: "No _id parameter provided"});
+                    } else {
+                        var query = {};
+                        query._id = objectid(req.params.id);
+                    }
+
+                    // Create GET _id query
+                    collection.find(query).toArray(function (err, data) {
+                        if (!err) {
+                            // 200 OK
+                            return res.status(200).send({success: true, code: 200, count: data.length, results: data});
+                        } else {
+                            // 500 Internal Server (Mongodb) Error
+                            console.log(err);
+                            return res.send({success: false, code: 500, error: "Internal Server Error"});
+                        }
+                    });
+                });
+
+                // Create Express POST route
+                app.post('/' + collections[route], function (req, res) {
+                    var dynamicQueryObj = {};
+                    // Set Mongo query object
+                    if (Object.keys(req.query).length > 0) {
+                        for (var p in req.query) {
+                            if (temp = req.query[p]) {
+                                if (!isNaN(temp + 0)) {
+                                    dynamicQueryObj[p] = parseInt(req.query[p]);
+                                } else {
+                                    dynamicQueryObj[p] = req.query[p];
+                                }
+                            }
+                        }
+                    } else {
+                        return res.status(400).send({success: false, code: 400, error: "No POST fields provided"});
+                    }
+
+                    // Create POST (insert) query
+                    collection.insert(dynamicQueryObj, function (err, data) {
+                        if (!err) {
+                            // 200 OK
+                            return res.status(200).send({success: true, code: 200, results: data});
+                        } else {
+                            // 500 Internal Server (Mongodb) Error
+                            console.log(err);
+                            return res.send({success: false, code: 500, error: "Internal Server Error"});
+                        }
+                    });
+                });
+
+                // Create Express PUT route
+                app.put('/' + collections[route] + '/:id', function (req, res) {
+                    // Return 400 Bad Request error if no params provided
+                    if (typeof req.params.id == 'undefined' || Object.keys(req.query).length == 0) {
+                        return res.status(400).send({success: false, code: 400, error: "No _id or request parameters provided"});
+                    }
+                    var dynamicQueryObj = {};
+                    // Set Mongo query object
+                    if (Object.keys(req.query).length > 0) {
+                        for (var p in req.query) {
+                            if (temp = req.query[p]) {
+                                if (!isNaN(temp + 0)) {
+                                    dynamicQueryObj[p] = parseInt(req.query[p]);
+                                } else {
+                                    dynamicQueryObj[p] = req.query[p];
+                                }
+                            }
+                        }
+                    }
+
+                    var query = {};
+                    var setFields = {};
+                    query._id = objectid(req.params.id);
+                    setFields.$set = dynamicQueryObj;
+                    console.log(setFields);
+
+                    // Create PUT _id query
+                    collection.update(query, setFields, function (err, data) {
+                        if (!err) {
+                            // 200 OK
+                            return res.status(200).send({success: true, code: 200, results: data});
+                        } else {
+                            // 500 Internal Server (Mongodb) Error
+                            console.log(err);
+                            return res.send({success: false, code: 500, error: "Internal Server Error"});
+                        }
+                    });
+                });
+            }
+        });
+    } else {
+        console.log('Configuration value "idroutes" set to false, default routes will not be loaded');
+    }
+}
+
+
+// Load admin-defined REST API routes from config table and create Express routes
+function loadAdminDefinedRoutes(app, configCollection, db, requireauth){
     configCollection.find({}).toArray(function (err, data) {
         if (!err) {
             // Process each route with data pulled from config collection
@@ -11,9 +139,10 @@ module.exports.loadRestRoutes = function(app, configCollection, db) {
                     var method = newData['method'];
                     var dynamicQuery = newData['query'];
                     var dynamicCollection = db.collection(newData['table']);
+                    var dynamicRoute = newData['route'];
 
                     // Create Express route
-                    app[method]('/' + newData['route'], function (req, res) {
+                    app[method]('/' + dynamicRoute, function (req, res) {
                         var dynamicQueryObj = {};
                         var queryOption = {}; // Empty for a GET or POST, must be used in a PUT to $SET updated fields
                         // Set Mongo query object
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var app_root = __dirname,
 express = require('express'),
 mongodb = require('mongodb'),
+objectid = mongodb.ObjectID,
 bodyParser = require('body-parser'),
 jade = require('jade'),
 iniparser = require('iniparser'),
@@ -17,20 +18,22 @@ rest = require('./routes/rest');
 // Parse configuration file using Iniparser module
 iniparser.parse('./config.ini', function(err,data){
     if (err){ console.log("An error occurred parsing the config.ini file");}
-    var host = data.host;
-    var db = data.database;
-    var configtable = data.configtable;
-    var serverport = data.serverport;
-    var dbport = data.dbport;
-    var dbuser = data.dbuser;
-    var dbpassword = data.dbpassword;
+    var host = data.host,
+    db = data.database,
+    configtable = data.configtable,
+    serverport = data.serverport,
+    dbport = data.dbport,
+    dbuser = data.dbuser,
+    dbpassword = data.dbpassword,
+    idroutes = data.idroutes,
+    requireauth = data.requireauth;
 
     console.log('Config.ini loaded. Host: %s:%d , Database: %s:%d , Config table: %s', host, serverport, db, dbport, configtable);
-    startServer(host, db, dbuser, dbpassword, serverport, dbport, configtable);
+    startServer(host, db, dbuser, dbpassword, serverport, dbport, configtable, idroutes, requireauth, objectid);
 });
 
 // Connect to database and build all routes
-function startServer(host, db, dbuser, dbpassword, serverport, dbport, configtable) {
+function startServer(host, db, dbuser, dbpassword, serverport, dbport, configtable, idroutes, requireauth, objectid) {
 
     // Create Express server with body-parser module
     var app = express();
@@ -54,18 +57,18 @@ function startServer(host, db, dbuser, dbpassword, serverport, dbport, configtab
     var connection = 'mongodb://' + dbuser + ':' + dbpassword + '@' + host + ':' + dbport + '/' + db;
 
     // Connect to Database and start REST API Server
-    mongodb.connect(connection, function(err, db) {
+    mongodb.connect(connection, function(err, database) {
         if (err) {
             console.log('There was an error connecting to MongoDB.');
         }
         console.log('Connection made to ' + connection);
 
         // Initialize connection to config table
-        var configCollection = db.collection(configtable);
+        var configCollection = database.collection(configtable);
 
         // Register Administration Console Routes and REST API routes
-        admin.loadAdminRoutes(app, configCollection, db);
-        rest.loadRestRoutes(app, configCollection, db);
+        admin.loadAdminRoutes(app, configCollection, database);
+        rest.loadRestRoutes(app, configCollection, database, configtable, idroutes, requireauth, objectid);
     });
 
     // Start server
@@ -73,7 +76,6 @@ function startServer(host, db, dbuser, dbpassword, serverport, dbport, configtab
         console.log('Express server on http://localhost:%d in %s mode\nCTRL + C to shutdown', serverport, app.settings.env);
     });
 }
-
 
 /*
  CONFIGURATION COLLECTION SCHEMA
